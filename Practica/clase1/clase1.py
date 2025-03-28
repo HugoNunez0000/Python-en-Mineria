3c8d6743da333d842d4ea135e2885e045ee5ae45
HugoNunez0000 <hugonunezp8@gmail.com>
2025-03-28 18:16:43 -0300

    creacion repositorio

---
+++ Practica/clase1/clase1.py
@@ -0,0 +1,34 @@
+print("Hola Mundo")
+print("chao Mundo")
+
+#Tipos de variable
+
+num = 30
+nombre ="Puro Kes"
+num1,num2 = 10, 20
+grav = 9.8
+
+my_lista = [1, 2, 9.8, "hola", 1000, [1, 2]] #Caja con mas cajas dentro
+my_tupla = (1, "chao", 20,4) #Similar a tupla la dif es que las listas se pueden cambiar
+           #|-----> ese elemento es el 0
+#las listas se pueden modificar mediente codigos y las tuplas nunca cambian, las tuplas son mas rapidas de ocupar ya que no cambian su contenido (el pc se demora menos)
+
+#diccionario dicc = {}
+
+numero = 20
+if numero == 20:
+    print("El numero es igual a 20")
+else:
+    print("El numero no es 20")
+
+for i in range(10):
+    print(i)
+
+var1 = 1
+while var1 < 100:
+    print("Puro Kes")
+    var1 = var1 + 1
+    
+    a = 400
+
+print(a)
